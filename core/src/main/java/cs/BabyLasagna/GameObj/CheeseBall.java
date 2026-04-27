e42233f636380adbb139d901bf85fc6f33549a8b
Mj <None>
2026-04-27 09:54:59 -0400

    Generalized projectile code a bit by creating Projectile class

--- core/src/main/java/cs/BabyLasagna/GameObj/CheeseBall.java
+++ core/src/main/java/cs/BabyLasagna/GameObj/CheeseBall.java
@@ -10,7 +10,7 @@ import cs.BabyLasagna.Game.GameInterface;
 import com.badlogic.gdx.utils.Array;
 import cs.BabyLasagna.TextureManager.Abilities.Cheese;
 
-public class CheeseBall extends GameObj {
+public class CheeseBall extends Projectile {
 
     public static final float BALL_SIZE    = 0.5f;
     public static final float SPLAT_WIDTH  = 6.0f / Game.PIXELS_PER_TILE;
@@ -21,22 +21,17 @@ public class CheeseBall extends GameObj {
     public static final float SOURCE_VEL = 0.5f;
 
     public static final float STICKY_VEL = 1.6f;
-    
-    private boolean facing_right = false;
-    private boolean splatted = false;
 
     public CheeseBall(GameInterface g, float x, float y, float vx, float vy, boolean facing_right) {
-        super(
-            g, x, y, 0.5f, 0.5f,
-            SOURCE_VEL*vx + (facing_right ? INITIAL_VX : -INITIAL_VX ),
-            SOURCE_VEL*vy + INITIAL_VY
-        );
+        super(g,x,y,vx,vy,SOURCE_VEL,facing_right);
 
-        this.facing_right = facing_right;
+        velocity.x += (this.facing_right ? INITIAL_VX : -INITIAL_VX);
+        velocity.y += INITIAL_VY;
     }
 
-    public void splat(Rectangle alignTile) {
-        splatted = true;
+    @Override
+    public void hit(Rectangle alignTile) {
+        stuck = true;
 
         hitbox.height = SPLAT_HEIGHT;
         hitbox.y = alignTile.y + 0.5f*(1.0f - hitbox.height);
@@ -46,26 +41,13 @@ public class CheeseBall extends GameObj {
         if (facing_right) hitbox.x += 1.0f;
     }
 
-    public boolean isSplatted() { return splatted; }
-
-    @Override
-    public void update(float deltaTime) {
-        if (splatted == true) {
-            // TODO: Make it follow solid objects that can move and check
-            // if the tile is still there. 
-            return;
-        }
-
-        velocity.y += GRAVITY*deltaTime;
-
-        moveWithCollisions(deltaTime);
-    }
+    public boolean isSplatted() { return stuck; }
 
     @Override
     public void render(float deltaTime, SpriteBatch batch) {
         TextureManager.draw(
             batch,
-            (splatted ? Cheese.getSplatTex() : Cheese.getGlobTex()),
+            (stuck ? Cheese.getSplatTex() : Cheese.getGlobTex()),
             hitbox.x,
             hitbox.y,
             hitbox.width,
@@ -75,65 +57,5 @@ public class CheeseBall extends GameObj {
         );
     }
 
-    // Move and collide with general list of hitboxes | Primary collision function
-    @Override
-    public void moveWithCollisions(Array<Rectangle> tile_rects, Vector2 movement_vec) {
-        grounded = false;
-
-        // Cap speed
-        if (Math.abs(movement_vec.x) > MAX_TILES_PER_FRAME) {
-            movement_vec.x = MAX_TILES_PER_FRAME * Math.signum(movement_vec.x);
-        }
-        if (Math.abs(movement_vec.y) > MAX_TILES_PER_FRAME) {
-            movement_vec.y = MAX_TILES_PER_FRAME * Math.signum(movement_vec.y);
-        }
-
-        // Handle x-movement and x-collisions first
-        hitbox.x += movement_vec.x;
-
-        Rectangle tileForAlign = null;
-
-        for (Rectangle tile : tile_rects) {
-            if (!hitbox.overlaps(tile)) continue;
-            tileForAlign = new Rectangle(tile);
-
-            float dx = (hitbox.x + hitbox.width/2f) - (tile.x + tile.width/2f);
-
-            if (dx > 0) {
-                hitbox.x = tile.x + tile.width;
-                facing_right = true;
-            }
-            else {
-                hitbox.x = tile.x - hitbox.width;
-                facing_right = false;
-            }
-        }
-
-        if (tileForAlign != null) {
-            splat(tileForAlign);
-            return;
-        }
-
-        // Handle y-movement and y-collisions last
-        hitbox.y += movement_vec.y;
-
-        for (Rectangle tile : tile_rects) {
-            if (!hitbox.overlaps(tile)) continue;
-
-            float dy = (hitbox.y + hitbox.height/2f) - (tile.y + tile.height/2f);
-
-            if (dy > 0) {
-                if (velocity.y <= 0) {
-                    hitbox.y = tile.y + tile.height;
-                    grounded = true;
-                }
-            } else {
-                if (velocity.y > 0) {
-                    hitbox.y = tile.y - hitbox.height;
-                }
-            }
-
-            velocity.y *= -0.8f; // Bounce off of top/bottom surface
-        }
-    }
+    
 }
---
+++ core/src/main/java/cs/BabyLasagna/GameObj/Projectile.java
@@ -0,0 +1,95 @@
+package cs.BabyLasagna.GameObj;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+import cs.BabyLasagna.Game.GameInterface;
+import com.badlogic.gdx.utils.Array;
+
+public abstract class Projectile extends GameObj {
+    protected boolean facing_right = false;
+    protected boolean stuck = false; // Whether the projectile is stuck onto something
+
+    public Projectile(GameInterface g, float x, float y, float vx, float vy, float source_vel, boolean facing_right) {
+        super(
+            g, x, y, 0.5f, 0.5f,
+            source_vel*vx,
+            source_vel*vy
+        );
+
+        this.facing_right = facing_right;
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        if (stuck) return;
+
+        velocity.y += GRAVITY*deltaTime;
+
+        moveWithCollisions(deltaTime);
+    }
+
+    protected abstract void hit(Rectangle target);
+
+    // Move and collide with general list of hitboxes | Primary collision function
+    @Override
+    public void moveWithCollisions(Array<Rectangle> tile_rects, Vector2 movement_vec) {
+        grounded = false;
+
+        // Cap speed
+        if (Math.abs(movement_vec.x) > MAX_TILES_PER_FRAME) {
+            movement_vec.x = MAX_TILES_PER_FRAME * Math.signum(movement_vec.x);
+        }
+        if (Math.abs(movement_vec.y) > MAX_TILES_PER_FRAME) {
+            movement_vec.y = MAX_TILES_PER_FRAME * Math.signum(movement_vec.y);
+        }
+
+        // Handle x-movement and x-collisions first
+        hitbox.x += movement_vec.x;
+
+        Rectangle tileForAlign = null;
+
+        for (Rectangle tile : tile_rects) {
+            if (!hitbox.overlaps(tile)) continue;
+            tileForAlign = new Rectangle(tile);
+
+            float dx = (hitbox.x + hitbox.width/2f) - (tile.x + tile.width/2f);
+
+            if (dx > 0) {
+                hitbox.x = tile.x + tile.width;
+                facing_right = true;
+            }
+            else {
+                hitbox.x = tile.x - hitbox.width;
+                facing_right = false;
+            }
+        }
+
+        if (tileForAlign != null) {
+            hit(tileForAlign);
+            return;
+        }
+
+        // Handle y-movement and y-collisions last
+        hitbox.y += movement_vec.y;
+
+        for (Rectangle tile : tile_rects) {
+            if (!hitbox.overlaps(tile)) continue;
+
+            float dy = (hitbox.y + hitbox.height/2f) - (tile.y + tile.height/2f);
+
+            if (dy > 0) {
+                if (velocity.y <= 0) {
+                    hitbox.y = tile.y + tile.height;
+                    grounded = true;
+                }
+            } else {
+                if (velocity.y > 0) {
+                    hitbox.y = tile.y - hitbox.height;
+                }
+            }
+
+            velocity.y *= -0.8f; // Bounce off of top/bottom surface
+        }
+    }
+}
